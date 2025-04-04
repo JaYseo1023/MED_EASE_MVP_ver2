337eb10d59b308e5390d43c15daabf158c0f4998
JaYseo1023 <jy.seo1023@gmail.com>
2025-04-04 18:10:04 +0900

    main

--- build.sh
+++ build.sh
@@ -9,17 +9,9 @@ cd frontend
 # Node.js 의존성 설치
 npm install
 
-<<<<<<< HEAD
 # TypeScript 컴파일 및 Vite 빌드 실행
 NODE_OPTIONS=--openssl-legacy-provider npx tsc
 NODE_OPTIONS=--openssl-legacy-provider npx vite build
-=======
-# TypeScript 컴파일 실행
-NODE_OPTIONS=--openssl-legacy-provider ./node_modules/.bin/tsc
-
-# Vite 빌드 실행
-NODE_OPTIONS=--openssl-legacy-provider ./node_modules/.bin/vite build
->>>>>>> ad2a13476865d69f81ce4848896314c694ec904f
 
 # 루트 디렉토리로 돌아가기
 cd ..
